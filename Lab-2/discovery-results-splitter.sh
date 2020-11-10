8c21847912f30e312ecefcd9baea30a423f09fb9
asad-mahmood1 <30564031+asad-mahmood1@users.noreply.github.com>
2020-11-10 08:23:24 -0500

    Update discovery-results-splitter.sh

--- Lab-2/discovery-results-splitter.sh
+++ Lab-2/discovery-results-splitter.sh
@@ -3,5 +3,5 @@ N=0;
 jq -c '.[]' DiscoveryOutput.json |
   while read -r json ; do
     N=$((N+1))
-    jq . <<< "$json" > "/Users/asadmahmood/Documents/WatsonCOVID19PoT/Lab2/DiscoveryResults/discovery-${N}.json"
+    jq . <<< "$json" > "/Users/username/Desktop/Watson-Lab2/DiscoveryResults/discovery-${N}.json"
   done
