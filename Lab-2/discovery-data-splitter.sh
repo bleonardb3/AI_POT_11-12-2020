dd60faecad7858c575ef8c0892542c8307fdf615
asad-mahmood1 <30564031+asad-mahmood1@users.noreply.github.com>
2020-11-10 08:20:09 -0500

    Delete discovery-data-splitter.sh

--- Lab-2/discovery-data-splitter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-N=0;
-jq -c '.[]' DiscoveryOutput.json |
-  while read -r json ; do
-    N=$((N+1))
-    jq . <<< "$json" > "/Users/<Mac username>/Documents/discovery-${N}.json"
-  done
